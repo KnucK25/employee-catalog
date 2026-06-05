7e5860dbe7c32c9b07eab08493dc35bac2af591b
6A6A3uHA <markisa130106@gmail.com>
2026-06-05 17:19:01 +0300

    Добавил комментарии пояснения/требования для каждого запроса к image

--- backend/src/database/queries/imageQueries.ts
+++ backend/src/database/queries/imageQueries.ts
@@ -1,20 +1,37 @@
 export const imageQueries = {
+
+  /**
+   * Принимает бинарный код изображения, миме тип изображения('jpeg', 'png', 'webp'), размер изображения.
+   * Добавляет изображение в бд.
+   */
   create: `
     INSERT INTO image (binary_image, mime_type, size_bytes)
     VALUES (?, ?, ?);
   `,
 
+  /**
+   * Принимает id изображения.
+   * Возвращает бинарный код изображения, миме тип('jpeg', 'png', 'webp'), размер изображения.
+   */
   getById: `
     SELECT id, binary_image, mime_type, size_bytes
     FROM image
     WHERE id = ?;
   `,
 
+  /**
+   * Принимает id изображения.
+   * Удаляет изображение.
+   */
   remove: `
     DELETE FROM image
     WHERE id = ?;
   `,
 
+  /**
+   * Принимает id профиля.
+   * Возвращает id изображения.
+   */
   getEmployeeImageId: `
     SELECT image_id
     FROM employee
