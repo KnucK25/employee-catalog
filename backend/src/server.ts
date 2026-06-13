9387c78d6f62f89401f25f88a34e5394fb3ecd1a
6A6A3uHA <markisa130106@gmail.com>
2026-06-14 01:15:36 +0300

    Решение конфликтов с бэкэндом

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,6 @@ import { initDB } from './database/init';
 import { Database } from 'sqlite';
 import * as path from 'path'; // Импортируем модуль для работы с путями
 import * as fs from 'fs'; // <-- Добавляем fs для чтения файлов
-//текст
 
 import * as crypto from 'crypto';
 
