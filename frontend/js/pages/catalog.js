0a30d05a7bb11195ede54fa9f1c0c5e6998e03ef
Alfheim0 <huckfupfub1998@gmail.com>
2026-06-14 16:33:06 +0300

    страничка прав доступа

--- frontend/js/pages/catalog.js
+++ frontend/js/pages/catalog.js
@@ -116,7 +116,6 @@ function renderCatalog(employeesList) {
 
     container.innerHTML = '';
     employeesList.forEach(emp => {
-        // ИСПОЛЬЗУЕМ ВАШИ КЛАССЫ ИЗ catalog.css
         container.innerHTML += `
             <div class="col-lg-4 col-md-6 mb-4">
                 <div class="employee-card">
@@ -127,9 +126,7 @@ function renderCatalog(employeesList) {
                         <h5 class="employee-name">${emp.name}</h5>
                         <p class="employee-position">${emp.post}</p>
                         <p class="employee-department">${emp.departament}</p>
-                        <button class="btn btn-details">
-                            <a href="card.html?id=${emp.id}">Подробнее</a>
-                        </button>
+                        <a href="card.html?id=${emp.id}" class="btn btn-details">Подробнее</a>
                     </div>
                 </div>
             </div>
